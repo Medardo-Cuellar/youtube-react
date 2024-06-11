a169152eee528c3ddf99446d30abdbb2cb9d47fc
Luis Medardo <soyriku@gmail.com>
2024-06-10 22:20:19 -0500

    se agrega aside y se completa navbar

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,3 +1,17 @@
+
 export default function Menu() {
-    return (<aside id="menu">MENU</aside>);
+    return <aside id="menu">
+        <div className="menu-item">
+            <span>🏠</span>
+            <span>Hong</span>
+            </div>
+        <div className="menu-item">
+            <span>🩳</span>
+            <span>Shorts</span>
+            </div>
+        <div className="menu-item">
+            <span>🕐</span>
+            <span>Subscriptions</span>
+            </div>
+    </aside>;
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,21 @@
+import YouTubeLogo from "./YouTubeLogo";
+
 export default function NavBar() {
-    return (
+  return (
     <nav id="navbar">
-        <div>YouTube</div>
-        <div>Search</div>
-        <div>User</div>
+      <div id="logo">
+        <YouTubeLogo height="1.8 rem" width="3rem" />
+        <span>YouTube</span>
+      </div>
+      <div id="search-container">
+        <input type="text" placeholder="search" />
+      </div>
+      <div id="user-icon">
+        <img
+          src="https://api.dicebear.com/8.x/notionists-neutral/svg?seed=Cuellar"
+          alt="user"
+        />
+      </div>
     </nav>
-);
+  );
 }
