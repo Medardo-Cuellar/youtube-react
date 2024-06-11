ab6de78de3085f1c7f2df0cacd64bdcdfd5f4087
Luis Medardo <soyriku@gmail.com>
2024-06-10 22:46:26 -0500

    se usa la funcion filter con list-rendering

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,15 @@
-
 export default function Menu() {
-    return <aside id="menu">
-        <div className="menu-item">
+  // list rendering
+  const menuItems = [
+    { icon: "🏠", label: "Home" },
+    { icon: "🩳", label: "Shorts" },
+    { icon: "🕐", label: "Subscriptions" },
+    { icon: "📺", label: "Library" },
+    { icon: "🌟", label: "Favorites" },
+  ];
+  return (
+    <aside id="menu">
+      {/* <div className="menu-item">
             <span>🏠</span>
             <span>Hong</span>
             </div>
@@ -12,6 +20,17 @@ export default function Menu() {
         <div className="menu-item">
             <span>🕐</span>
             <span>Subscriptions</span>
+            </div> */}
+      {menuItems
+        .filter((item) => item.label.startsWith("S"))
+        .map((item) => {
+          return (
+            <div key={`menu-item=${item.text}`} className="menu-item">
+              <span>{item.icon}</span>
+              <span>{item.label}</span>
             </div>
-    </aside>;
+          );
+        })}
+    </aside>
+  );
 }
